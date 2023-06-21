31a55474ec199a66d2c94b52edff708c3b49ab27
NoahVW <136670476+NoahVW@users.noreply.github.com>
2023-06-21 18:33:54 -0500

    program files

---
+++ ListException.java
@@ -0,0 +1,17 @@
+/**
+ * class ListException
+ *
+ *    A class that implements the ListException class
+ *
+ */
+
+public class ListException extends RuntimeException
+{
+  	public ListException(String s)
+  	{
+    	super(s);
+  	}  // end constructor
+
+  	private final static long serialVersionUID = 2008L;
+
+}  // end ListException
